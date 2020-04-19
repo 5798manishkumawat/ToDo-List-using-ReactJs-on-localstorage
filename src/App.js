53be3af5e2188ebc0c0d3d4ebc73379adeafb57d
Manish Kumawat <5798manishkumawat@gmail.com>
2020-04-20 00:58:06 +0530

    to-do list

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import LOGIN from "./componenets/login";
+
+function App() {
+  return (
+    <div class="bg">
+      <LOGIN />
+    </div>
+  );
+}
+
+export default App;
